b7c2103280181c8bd9afa9cfe7305820ae45f851
clem <493151+cervelas@users.noreply.github.com>
2023-03-10 16:53:23 +0100

    lot of updates

--- movx/core/dcp.py
+++ movx/core/dcp.py
@@ -7,6 +7,7 @@ from movx.core.location import Location
 
 import clairmeta
 
+
 class DCP:
     def __init__(self, folder, rel_to, location):
         self.path = Path(folder)
@@ -35,8 +36,8 @@ class DCP:
     def parse(self):
         try:
             self.dcp._parsed = False
-            self.dcp._probeb = True
-            self.metadata = self.dcp.parse()
+            self.dcp._probeb = False
+            self.metadata = self.dcp.parse(probe=True)
             self.package_type = self.metadata.get("package_type", "Unknown")
             self.size = self.metadata.get("size", "Unknown")
 
@@ -47,17 +48,22 @@ class DCP:
         except Exception as e:
             print(e)
 
-    def check(self, ov=None, cb=None):
-        status, report = self.dcp.check( ov_path=ov,
-        hash_callback=cb)
-        
-        for c in report.checks_failed():
-            pprint.pprint(c.short_desc())
-            pprint.pprint([ e.message for e in c.errors ])
+    def check(self, profile=None, cb=None):
+        report = {}
+        profile = profile or CHECK_PROFILE
+        try:
+            status, report = self.dcp.check(profile=CHECK_PROFILE, ov_path=self.ov_path,
+            hash_callback=cb)
+            
+            for c in report.checks_failed():
+                pprint.pprint(c.short_desc())
+                pprint.pprint([ e.message for e in c.errors ])
 
 
-        self.report = report.to_dict()
-
+            self.report = report.to_dict()
+        except Exception as e:
+            report['Message'] = "DCP Check failed:" + str(e)
+            self.report = report
         return report
 
     def to_dict(self):
@@ -96,3 +102,42 @@ class DCP:
         except Exception as e:
             print(e)
             return dcp
+
+CHECK_PROFILE = {
+    'criticality': {
+        'default': 'ERROR',
+        'check_dcnc_': 'WARNING',
+        'check_*_xml': 'WARNING', # MOVX Custom
+        'check_subtitle_cpl_language': 'WARNING', # MOVX Custom
+        'check_dcp_foreign_files': 'WARNING',
+        'check_assets_am_volindex_one': 'WARNING',
+        'check_*_empty_text_fields': 'WARNING',
+        'check_*_empty_text_fields_missing': 'ERROR',
+        'check_*_xml_constraints_line_ending': 'WARNING',
+        'check_cpl_contenttitle_annotationtext_match': 'WARNING',
+        'check_cpl_contenttitle_pklannotationtext_match': 'WARNING',
+        'check_assets_cpl_missing_from_vf': 'WARNING',
+        'check_assets_cpl_labels_schema': 'WARNING',
+        'check_assets_cpl_filename_uuid': 'WARNING',
+        'check_certif_multi_role': 'WARNING',
+        'check_certif_date_overflow': 'WARNING',
+        'check_picture_cpl_avg_bitrate': 'WARNING',
+        'check_picture_cpl_resolution': 'WARNING',
+        'check_subtitle_cpl_reel_number': 'WARNING',
+        'check_subtitle_cpl_empty': 'WARNING',
+        'check_subtitle_cpl_uuid_case': 'WARNING',
+        'check_subtitle_cpl_duplicated_uuid': 'WARNING',
+        'check_subtitle_cpl_first_tt_event': 'WARNING',
+        'check_picture_cpl_archival_framerate': 'WARNING',
+        'check_picture_cpl_hfr_framerate': 'WARNING',
+        'check_sound_cpl_format': 'WARNING',
+        'check_sound_cpl_channel_assignments': 'WARNING',
+        'check_atmos_cpl_channels': 'WARNING',
+        'check_atmos_cpl_objects': 'WARNING',
+    },
+    # Checker options
+    # Bypass is a list of check names (function names)
+    'bypass': [
+        #'check_assets_pkl_hash'
+    ],
+}
