91339770890fbd178816b04e0a8d299b9a2b79f5
clem <493151+cervelas@users.noreply.github.com>
2024-03-01 10:21:53 +0100

    selfupdate fix

--- movx/selfupdate.py
+++ movx/selfupdate.py
@@ -5,16 +5,16 @@ from pathlib import Path
 def update():
     cwd = Path(__file__).parent.parent
     print("updating movx from git.")
-    subprocess.call(["git", "status"], cwd=cwd, shell=True)
-    subprocess.call(["git", "pull"], cwd=cwd, shell=True)
-    subprocess.call(["git", "status"], cwd=cwd, shell=True)
+    subprocess.call(["git", "status"], cwd=cwd, shell=False)
+    subprocess.call(["git", "pull"], cwd=cwd, shell=False)
+    subprocess.call(["git", "status"], cwd=cwd, shell=False)
     print("update pip")
-    subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], cwd=cwd, shell=True)
+    subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], cwd=cwd, shell=False)
     print("reinstall movx python package.")
-    subprocess.call([sys.executable, "-m", "pip", "install", "-e", "."], cwd=cwd, shell=True)
+    subprocess.call([sys.executable, "-m", "pip", "install", "-e", "."], cwd=cwd, shell=False)
     print("restarting service")
     
-    subprocess.call(["service", "movx", "restart"], cwd=cwd, shell=True)
+    subprocess.call(["service", "movx", "restart"], cwd=cwd, shell=False)
     exit(0)
 
 if __name__ == "__main__":
